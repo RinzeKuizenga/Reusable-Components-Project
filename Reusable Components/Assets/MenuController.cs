91aabe94abbd8f4ecafdf6df45b5a9a00df229e2
RinzeKuizenga <144996520+RinzeKuizenga@users.noreply.github.com>
2026-05-25 23:34:02 +0200

    wad

--- Reusable Components/Assets/MenuController.cs
+++ Reusable Components/Assets/MenuController.cs
@@ -6,5 +6,29 @@ public class MenuController : MonoBehaviour
     string[] options = { "Attack", "Items", "Supers" };
     int selectedIndex = 0;
 
+    private void Start()
+    {
+         
+    }
+    void MoveRight()
+    {
+        options[selectedIndex] += 1;
+    }
+    void MoveLeft()
+    {
+        options[selectedIndex] += 1;
+    }
 
+    private void Update()
+    {
+        Debug.Log($"{options[selectedIndex]}");
+        if (Input.GetKey(KeyCode.A))
+        {
+            MoveLeft();
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            MoveRight();
+        }
+    }
 }
